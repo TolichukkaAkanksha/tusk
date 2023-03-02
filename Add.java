505f9d12eb58394f060b7890b1a894e776e18d08
Tolichukka Akanksha <125852692+TolichukkaAkanksha@users.noreply.github.com>
2023-03-02 12:47:20 +0530

    initial commit

---
+++ Add.java
@@ -0,0 +1,13 @@
+class Addition{
+  public static void main(String arg[])
+  {  int a=10;
+     int b=20;
+     add(a,b);
+  }
+  static void add(int c,int d)
+  {
+    int sum=c+d;
+    System.out.println(sum);
+  }
+  
+}
